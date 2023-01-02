bd6a702519d367a605f3c64f7909d2c20221444a
danosavi <1233251+danosavi@users.noreply.github.com>
2023-01-02 14:08:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  console.log(req.file);
+  let file = req.file;
+  res.json({name:file.originalname, type:file.mimetype, size: file.size})
+  
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
